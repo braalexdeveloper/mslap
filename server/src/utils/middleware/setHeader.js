e22a6e7e93f4c4df1cb284922dade52a98627ff3
Diego López Beltrán <89640777+Saint-Diego@users.noreply.github.com>
2023-05-29 13:36:59 -0500

    Update setHeader.js
    Acept all url origins

--- server/src/utils/middleware/setHeader.js
+++ server/src/utils/middleware/setHeader.js
@@ -1,5 +1,5 @@
 const setHeader = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mslaps.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
