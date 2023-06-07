f9569b178cbc78f89b9f0ec18415777010dbc0cc
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-06-07 17:25:23 -0500

    ident code

--- server/src/controllers/certificateController.js
+++ server/src/controllers/certificateController.js
@@ -1,6 +1,6 @@
-const { Certificate, User,Role,Project } = require("../db");
+const { Certificate, User, Role, Project } = require("../db");
 const { Op } = require("sequelize");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const certificateController = {
   getObservation: async (req, res) => {
@@ -9,113 +9,115 @@ const certificateController = {
 
       const responseObservation = await Certificate.findOne({ where: { id } });
 
-      res.status(200).json(responseObservation)
+      res.status(200).json(responseObservation);
     } catch (err) {
       res.status(500).json({ err });
-      console.log(err)
+      console.log(err);
     }
-
   },
-  validateCertificate:async(req,res)=>{
+  validateCertificate: async (req, res) => {
     try {
-     const { id } = req.params;
+      const { id } = req.params;
       const { isValid } = req.body;
 
-      await Certificate.update({ isValid }, {
-        where: {
-          id
+      await Certificate.update(
+        { isValid },
+        {
+          where: {
+            id,
+          },
         }
-      });
+      );
 
       res.status(200).json({
         status: 1,
-        message: "Se valido Certificado"
-                
-        })
-
-      } catch (err) {
+        message: "Se valido Certificado",
+      });
+    } catch (err) {
       res.status(500).json({ err });
-      console.log(err)
+      console.log(err);
     }
-
   },
   addObservation: async (req, res) => {
     try {
       const { id } = req.params;
       const { observation } = req.body;
 
-      await Certificate.update({ observation }, {
-        where: {
-          id
-        }
-      });
-
-      const userProjectCertificate=await Certificate.findAll({
-        where:{id},
-        include:[
+      await Certificate.update(
+        { observation },
         {
-          model:User,
-          include:{
-           model:Project,
-           }
+          where: {
+            id,
+          },
         }
-          ]
-        
+      );
+
+      const userProjectCertificate = await Certificate.findAll({
+        where: { id },
+        include: [
+          {
+            model: User,
+            include: {
+              model: Project,
+            },
+          },
+        ],
       });
 
-const nameOperario=userProjectCertificate[0].user.name+" "+userProjectCertificate[0].user.lastName;
-      const nameProjectOperario=userProjectCertificate[0].user.projects[0].name;
+      const nameOperario =
+        userProjectCertificate[0].user.name +
+        " " +
+        userProjectCertificate[0].user.lastName;
+      const nameProjectOperario =
+        userProjectCertificate[0].user.projects[0].name;
 
       //Obteniendo los correos de usuarios admin y contratistas
-      const userAdminByRole=await Role.findAll({
-        where:{
-              value:"admin"
-            },
-        include:User
-               
-      });
-
-let emailAdmin=userAdminByRole[0].users[0].email;
-
-     const usuersByRole=await Role.findAll({
-        where:{
-              value:"contratista"
-            },
-        include:[
-        {
-           model:User,
-           include:{
-           model:Project,
-           }
+      const userAdminByRole = await Role.findAll({
+        where: {
+          value: "admin",
         },
-        
-        ]
+        include: User,
       });
-console.log(usuersByRole)
 
-      let emailsContratistas=usuersByRole[0].users.map(el=>{
-          if(el.projects.map(ele=>ele.name).includes(nameProjectOperario)){
-            return el.email
-           }
-                           
-        })
+      let emailAdmin = userAdminByRole[0].users[0].email;
 
-      let emailRefactor=emailsContratistas.filter(element=>element!=null);
+      const usuersByRole = await Role.findAll({
+        where: {
+          value: "contratista",
+        },
+        include: [
+          {
+            model: User,
+            include: {
+              model: Project,
+            },
+          },
+        ],
+      });
+      console.log(usuersByRole);
 
-      let emails=[...emailRefactor,emailAdmin];
-      
-   
-     
-     const transporter = nodemailer.createTransport({
+      let emailsContratistas = usuersByRole[0].users.map((el) => {
+        if (el.projects.map((ele) => ele.name).includes(nameProjectOperario)) {
+          return el.email;
+        }
+      });
+
+      let emailRefactor = emailsContratistas.filter(
+        (element) => element != null
+      );
+
+      let emails = [...emailRefactor, emailAdmin];
+
+      const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         auth: {
-          user:process.env.EMAIL_NODEMAILER,
-          pass:process.env.PASSWORD
-        }
+          user: process.env.EMAIL_NODEMAILER,
+          pass: process.env.PASSWORD,
+        },
       });
-      
-     let plantillaHtml=`
+
+      let plantillaHtml = `
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
@@ -142,7 +144,6 @@ console.log(usuersByRole)
 
     }
 
-
     .container {
       background-color: rgb(255, 255, 255);
       width: 60%;
@@ -150,7 +151,6 @@ console.log(usuersByRole)
       text-align: center;
       padding-bottom: 1rem;
     }
-
 
     .btnIniciarSesion {
       background-color: rgb(21, 52, 138);
@@ -169,7 +169,6 @@ console.log(usuersByRole)
 
 <body>
   <div class="container">
-
     <img
       src='https://res.cloudinary.com/dwmrbilbo/image/upload/v1684120167/image-5_rrmaj0.png' />
     <div>
@@ -177,39 +176,35 @@ console.log(usuersByRole)
       <p>Hola, tienes una nueva observación en el operario <b>${nameOperario} con DNI: ${userProjectCertificate[0].user.dni}</b></p>
       <a href='https://system-mslaps.vercel.app/' class="btnIniciarSesion" target='_blank'>Iniciar Sesión</a>
     </div>
-
   </div>
 </body>
-
 </html>
 `;
 
       const mailOptions = {
-        from:process.env.EMAIL_NODEMAILER,
+        from: process.env.EMAIL_NODEMAILER,
         to: emails.toString(),
-        subject: 'Notificación de Observación en MSLAPS',
-        html:plantillaHtml 
+        subject: "Notificación de Observación en MSLAPS",
+        html: plantillaHtml,
       };
 
       transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
           console.log(error);
         } else {
-          console.log('Email sent: ' + info.response);
+          console.log("Email sent: " + info.response);
         }
       });
 
       res.status(200).json({
         status: 1,
-        message: "Observación Agregada"
-                
-        })
+        message: "Observación Agregada",
+      });
     } catch (err) {
       res.status(500).json({ err });
-      console.log(err)
+      console.log(err);
     }
-
-  }
-}
+  },
+};
 
 module.exports = certificateController;
